a926bc235fca2c35d072048fd68949de8b682d9f
aryasingh-ops <m265844@usna.edu>
2023-10-12 09:44:54 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,2 +1,16 @@
 import pygame
-import math
+import time
+pygame.init()
+
+print(f"the quit event in type {pygame.QUIT}")
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption("Chomp!")
+screen.fill((119, 28, 156))
+pygame.draw.rect(screen,(100,25,0) , (0, 380, 400, 400) )
+pygame.display.flip()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("NO YOU CANNOT ESCAPE MY PYGAME!!!")
+        #print(event.type)
+
