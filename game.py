b7be2d28b2031332d28333e13e0871c3c5ba62e9
aryasingh-ops <m265844@usna.edu>
2023-10-13 09:39:45 -0400

    basic game working

--- game.py
+++ game.py
@@ -6,11 +6,13 @@ print(f"the quit event in type {pygame.QUIT}")
 screen = pygame.display.set_mode((400, 400))
 pygame.display.set_caption("Chomp!")
 screen.fill((119, 28, 156))
-pygame.draw.rect(screen,(100,25,0) , (0, 380, 400, 400) )
-pygame.display.flip()
-while True:
+pygame.draw.rect(screen,(100,25,0) , (0, 380, 400, 400) ) #anchor point, height, width
+pygame.display.flip() #the screen works like stop-motion. there is a ping pong buffer where you draw on one and display on the other
+while True: #The loop defines your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print("NO YOU CANNOT ESCAPE MY PYGAME!!!")
+           # print("NO YOU CANNOT ESCAPE MY PYGAME!!!")
+            print("thanks for playing!")
+            pygame.quit()
+            sys.exit()
         #print(event.type)
-
