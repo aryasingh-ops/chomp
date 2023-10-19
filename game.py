6f0df3f76f02a0c9fc1fdc2fb1e1e6aba471463c
aryasingh-ops <m265844@usna.edu>
2023-10-19 08:52:56 -0400

    added game fonts!

--- game.py
+++ game.py
@@ -1,31 +1,37 @@
 import pygame
+import random
+
+from settings import *
 
 pygame.init()
-# let's define variables that make the code scale better/ get rid of teal:
-TILE_SIZE = 64  # tiles are square height == width
 
-SCREEN_WIDTH = 8 * TILE_SIZE
-SCREEN_HEIGHT = 8 * TILE_SIZE
-SAND_HEIGHT = 20
-
-WATER_COLOR = (114, 159, 232)
-SAND_COLOR = (100, 25, 0)
+game_font = pygame.font.Font("assets/fonts/Ocean Coastlines.otf", 128)
 
 print(f"the quit event in type {pygame.QUIT}")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
 screen.fill(WATER_COLOR)
-pygame.draw.rect(screen, SAND_COLOR,
-                 (0, SCREEN_HEIGHT - SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))  # anchor point, height, width
+
 sand = pygame.image.load("assets/images/sand.png").convert()  # load the image then blit it to the screen
-
+sand_top = pygame.image.load("assets/images/sand_top.png").convert()
+sand_top.set_colorkey((0, 0, 0))
+seaweed = pygame.image.load("assets/images/seagrass.png").convert()
+seaweed.set_colorkey((0, 0, 0))
 # blit sand tiles across the bottom of the screen:
-
 for i in range(SCREEN_WIDTH // TILE_SIZE):
-    screen.blit(sand, (TILE_SIZE * i, SCREEN_HEIGHT-(2*TILE_SIZE)))
+    screen.blit(sand, (TILE_SIZE * i, SCREEN_HEIGHT - TILE_SIZE))
+    screen.blit(sand_top, (TILE_SIZE * i, SCREEN_HEIGHT - (2 * TILE_SIZE)))
 
-screen.blit(sand, (SCREEN_WIDTH // 2 - TILE_SIZE // 2,
-                   SCREEN_HEIGHT // 2 - TILE_SIZE // 2))
+# randomly place 5 pieces of grass along the bottom of the screen
+for _ in range(5):
+    x = random.randint(0, SCREEN_WIDTH)
+    # offset the seaweed so it looks better
+    y = random.randint(SCREEN_HEIGHT - 3 * TILE_SIZE, SCREEN_HEIGHT) + (0.5 * TILE_SIZE)
+    screen.blit(seaweed, (x, y))
+# Draw the CHOMP! title
+text = game_font.render("CHOMP!", True, (5, 31, 125))
+text.get_width()
+screen.blit(text, (SCREEN_WIDTH//2-text.get_width()//2, SCREEN_HEIGHT//2))
 
 pygame.display.flip()  # the screen works like stop-motion.
 # there is a ping pong buffer where you draw on one and display on the other
---
+++ settings.py
@@ -0,0 +1,9 @@
+# let's define variables that make the code scale better/ get rid of teal:
+TILE_SIZE = 64  # tiles are square height == width
+
+SCREEN_WIDTH = 12 * TILE_SIZE
+SCREEN_HEIGHT = 8 * TILE_SIZE
+SAND_HEIGHT = 20
+
+WATER_COLOR = (114, 159, 232)
+SAND_COLOR = (100, 25, 0)
